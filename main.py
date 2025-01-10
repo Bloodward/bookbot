180a615ce5271a30bb5299c199605e91a58d2bf2
Bloodward <Kevfry12@gmail.com>
2025-01-10 17:12:21 -0600

    added function for letter count

--- main.py
+++ main.py
@@ -2,12 +2,28 @@ def main():
     book_path = "/home/bloodward/workspace/github.com/Bloodward/bookbot/books/Frankenstein.txt"
     book_text = get_book_text(book_path)
     book_num_words = get_num_words(book_text)
+    book_letter_count = get_letter_count(book_text)
 
-    print(f'{book_num_words} words found in the document')
+    print(book_letter_count)
+
+
+
+    #print(f'{book_num_words} words found in the document')
 
 
     #Prints entire book out. 
     #print(book_text)
+
+def get_letter_count(text):
+    characters = {}
+    for c in text:
+        lower_char = c.lower()
+        if lower_char in characters:
+            characters[lower_char] += 1
+        else:
+            characters[lower_char] = 1
+    return characters
+
 
 def get_num_words(text):
     words = text.split()
